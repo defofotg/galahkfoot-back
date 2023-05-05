fa8bff11f805d0892e9a2d3d9833c5420fa9625c
Georges DEFO <defo.georges@gmail.com>
2023-05-05 23:37:52 +0200

    initial commit

---
+++ src/main/java/fr/enaiconseil/galahkfoot/GalaHkFootApplication.java
@@ -0,0 +1,13 @@
+package fr.enaiconseil.galahkfoot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GalaHkFootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GalaHkFootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/enaiconseil/galahkfoot/GalaHkFootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.enaiconseil.galahkfoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GalaHkFootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
